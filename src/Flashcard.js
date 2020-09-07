ee8d2b6e952f37e81a4323c26900117da6fe94da
dolev146 <dolev146@gmail.com>
2020-09-07 14:12:43 -0700

    3 files modified

--- src/Flashcard.js
+++ src/Flashcard.js
@@ -1,51 +1,29 @@
 import React, { useState, useEffect, useRef } from 'react'
 
-export default function Flashcard({ flashcard }) {
-    const [flip, setFlip] = useState(false)
-    const [height, setHeight] = useState("initial")
-    const [trueSideCards, settrueSideCards] = useState([])
-
+export default function Flashcard({ flashcard, trueSideCards, settrueSideCards , handleClick , flip, setFlip , index}) {
+    
+    
     const frontEl = useRef()
     const backEl = useRef()
 
-    function setMaxHeight() {
-        const frontHeight = frontEl.current.getBoundingClientRect().height
-        const backHeight = backEl.current.getBoundingClientRect().height
-        setHeight(Math.max(frontHeight, backHeight, 100))
-    }
+    
 
-    useEffect(() => {
-        console.log("render")
-        console.log(flip)
-        console.log(trueSideCards)
-    })
+    
 
-    const handleClick = () => {
-        setFlip(!flip)
-        settrueSideCards(prevtrueSideCards => {
-            const newArray = [...prevtrueSideCards]
-            console.log(flashcard.answer)
-            newArray.push(flashcard.answer)
-            return newArray
-        })
-    }
+    
 
-    useEffect(setMaxHeight, [flashcard.question, flashcard.answer, flashcard.options])
-    useEffect(() => {
-        window.addEventListener('resize', setMaxHeight)
-        return () => window.removeEventListener('resize', setMaxHeight)
-    }, [])
+    
     return (
         <div
             className={`card ${flip ? 'flip' : ''}`}
-            style={{ height: height }}
-            onClick={handleClick}
+            style={{ height: "200px" }}
+            onClick={() =>handleClick(index , flashcard.id)}
         >
             <div className="front" ref={frontEl}>
-                <img alt="" src={flashcard.question} width="180px"  ></img>
+                <img alt="" src={flashcard.question} width="180px"   ></img>
             </div>
             <div className="back" ref={backEl}>
-                <img alt="" src={flashcard.answer} width="180px" ></img>
+                <img alt="" src={flashcard.answer} width="180px" height="180px" ></img>
             </div>
         </div>
     )
--- src/FlashcardList.js
+++ src/FlashcardList.js
@@ -1,11 +1,48 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
 import Flashcard from './Flashcard'
 
+let beforeRenderFlip = []
+
 export default function FlashcardList({ flashcards }) {
+
+    const [trueSideCards, settrueSideCards] = useState([])
+    const [flip, setFlip] = useState(Array(flashcards.length - 1).fill(false))
+
+
+    useEffect(() => {           
+        if (trueSideCards.length === 2) {
+            if (trueSideCards[0] === trueSideCards[1]) {
+                console.log(flip)
+                beforeRenderFlip=[...flip]
+            } else {
+                let coverCard = setTimeout(() => {
+                    setFlip(beforeRenderFlip)
+                }, 500)
+            }
+            trueSideCards.length = 0
+        }
+        
+    }, [trueSideCards])
+
+
+    const handleClick = (index, id) => {
+        const tempArray = [...flip];
+        tempArray[index] = !tempArray[index]
+        setFlip(tempArray)
+        settrueSideCards([...trueSideCards, flashcards[index].answer])
+    }
     return (
         <div className="card-grid" >
-            {flashcards.map(flashcard => {
-                return <Flashcard flashcard={flashcard} key={flashcard.id} />
+            {flashcards.map((flashcard, index) => {
+                return <Flashcard
+                    index={index}
+                    flip={flip[index]}
+                    setFlip={setFlip}
+                    handleClick={handleClick}
+                    settrueSideCards={settrueSideCards}
+                    trueSideCards={trueSideCards}
+                    flashcard={flashcard}
+                    key={flashcard.id} />
             })}
         </div>
     )
